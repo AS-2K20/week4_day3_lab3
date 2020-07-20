127fc043494f8807dda953ab50b07f86f7fe0516
AS-2K20 <amudhan.psgcas@gmail.com>
2020-07-20 07:41:12 +0530

    Displaying in List

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,19 @@
+package controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main{
+	static List<Skill> listSkills= new ArrayList<Skill>(); 
+	
+	public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
+		System.out.println("List of all Skills");
+		SkillDAO skillDao = new SkillDAO();
+		listSkills=skillDao.listAllSkills();
+		}
+	}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,46 @@
+package  dao;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+	
+	public List <Skill> listAllSkills () throws ClassNotFoundException, IOException, SQLException
+	{
+		List<Skill> listSkills= new ArrayList<Skill>(); 
+		Long Id = null;
+		String Name = null;
+		Skill skill = new Skill(Id,Name);
+		try {
+		
+		Connection con = ConnectionManager.getConnection();
+		String selectSql = "SELECT SKILLID,SKILLNAME FROM SKILL";
+		Statement st = con.createStatement();
+		ResultSet resultSet = st.executeQuery(selectSql);
+		while(resultSet.next()) {
+			Long skillId = resultSet.getLong("skillId");
+			String skillName= resultSet.getString("skillName");
+			System.out.println(skillId+". "+skillName);
+			skill.setSkillId(skillId);
+			skill.setSkillName(skillName);
+			listSkills.add(skill);
+		}
+		con.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return listSkills;
+	
+	}
+}
